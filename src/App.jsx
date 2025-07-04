0f8314d39f652fd287dcde4dcdceec2ae135e36c
Aditya Garg <adityagarg733@gmail.com>
2025-07-04 22:44:05 +0530

    rooms page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import { useLocation } from 'react-router-dom'
+import Home from './pages/Home' 
+
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("owner");
   return (
     <div>
       {!isOwnerPath && <Navbar />}
-      <Home></Home>
+      <Home/>
     </div>
   )
 }
