41d71a4d052836f848bb26a57817f99cc4b17213
Aditya Garg <adityagarg733@gmail.com>
2025-07-29 18:00:34 +0530

    backend commits

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import connectDB from "./configs/db.js";
 import { clerkMiddleware } from '@clerk/express' 
 import clerkWebhooks from "./controllers/clerkWebhooks.js";
+import userRouter from "./routes/userRoutes.js"; 
 
 connectDB();
 
@@ -16,6 +17,7 @@ app.use("/api/clerk", clerkWebhooks);
 
 
 app.get('/', (req, res) => res.send("API is working"));
+app.use("/api/user", userRouter);
 
 const PORT = process.env.PORT || 3000;
 
