d2cbf0709f667739e235a35b4d594d9dd282c139
Aditya Garg <adityagarg733@gmail.com>
2025-08-14 19:13:06 +0530

    some more fixes

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,8 +1,8 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { AppProvider } from './context/AppContext.jsx'
 import { ClerkProvider } from '@clerk/clerk-react'
 
 // Import your Publishable Key
@@ -15,7 +15,9 @@ if (!PUBLISHABLE_KEY) {
 createRoot(document.getElementById('root')).render(
   <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
     <BrowserRouter>
-      <App />
+      <AppProvider>
+        <App />
+      </AppProvider>
     </BrowserRouter>
-  </ClerkProvider>,
+  </ClerkProvider>
 )
